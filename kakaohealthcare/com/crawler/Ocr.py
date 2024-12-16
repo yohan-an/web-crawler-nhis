26261c7f398cbedb1e25fd6ae658d23c840d937c
yohan.an <an.sangkil@gmail.com>
2024-12-16 18:02:16 +0900

    feat: 텍스트간 유사도측정 기능 추가

--- kakaohealthcare/com/crawler/Ocr.py
+++ kakaohealthcare/com/crawler/Ocr.py
@@ -1,4 +1,6 @@
 import os
+from idlelib.browser import file_open
+
 from google.cloud import vision
 from pdf2image import convert_from_path
 import io
@@ -8,13 +10,19 @@ class Ocr(object):
     def __init__(self):
         pass
 
-    def detect_text(self, path):
-        """Detects text in the file."""
+    def ocr_pdf_images(self, paths):
+        for path in paths:
+            self.detect_text(path)
+
+
+    def detect_text(self, image_path):
+        """
+            이미지 텍스트 변환
+        """
         # Google Cloud Vision 클라이언트 초기화
         client = vision.ImageAnnotatorClient()
-
         # 이미지 파일 읽기
-        with io.open(path, 'rb') as image_file:
+        with io.open(image_path, 'rb') as image_file:
             content = image_file.read()
 
         # Vision API에 전달할 이미지 객체 생성
@@ -28,8 +36,12 @@ class Ocr(object):
         # 감지된 텍스트 출력
         if texts:
             # 첫 번째 어노테이션은 전체 텍스트를 포함
-            print('전체 텍스트:')
+            print(f"--- OCR 결과 ({image_path}) ---")
             print(texts[0].description)
+            print("\n")
+
+            ground_truth = self.file_read_to_text(file_path = "/Users/yohan.an/git/crawling/web-data-crawler/web-crawler-nhis/text/original_sample.txt")
+            accuracy = self.calculate_accuracy(ground_truth, texts[0].description)
 
             # 개별 텍스트 블록 출력
             # print('\n개별 텍스트 블록:')
@@ -42,10 +54,7 @@ class Ocr(object):
 
         # 오류 처리
         if response.error.message:
-            raise Exception(
-                '{}\nFor more info on error messages, check: '
-                'https://cloud.google.com/apis/design/errors'.format(
-                    response.error.message))
+            raise Exception('{}\nFor more info on error messages, check: ''https://cloud.google.com/apis/design/errors'.format(response.error.message))
 
     def pdf_to_images(self, pdf_path, output_folder='pdf_images'):
         """PDF 파일을 이미지로 변환"""
@@ -65,6 +74,23 @@ class Ocr(object):
         return image_paths
 
 
+    def calculate_accuracy(self, ground_truth, ocr_result):
+        from difflib import SequenceMatcher
+
+        matcher = SequenceMatcher(None, ground_truth, ocr_result)
+        accuracy = matcher.ratio() * 100
+
+        print(f"OCR 정확도: {accuracy:.2f}%")
+
+
+
+    def file_read_to_text(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        return content
+
+
 def main():
     # PDF 파일 경로 설정
     pdf_path_file = '/Users/yohan.an/git/crawling/web-data-crawler/web-crawler-nhis/pdf/3-20241211134336726.pdf'
@@ -81,13 +107,18 @@ def main():
     # 디버깅: 예외 처리
     try:
         # OCR 함수 호출
-        image_paths = ocr.pdf_to_images(pdf_path_file)
+        image_paths = ocr.pdf_to_images(pdf_path_file, "/Users/yohan.an/git/crawling/web-data-crawler/web-crawler-nhis/images")
         if image_paths:
-            ocr.detect_text(image_paths[0])
+            #ocr.detect_text(image_paths[0])
+            ocr.ocr_pdf_images(image_paths)
     except Exception as e:
         print("오류 발생:", str(e))
         import traceback
         traceback.print_exc()
+
+
+
+
 
 
 if __name__ == '__main__':
